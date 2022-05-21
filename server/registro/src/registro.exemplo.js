a41ffda6e3ad4138ba57bb034c9fe29dd9f17381
João Pedro Luz <joao.pedro.luz@hotmail.com>
2022-05-20 21:42:10 -0300

    Adicionado exemplo de json para registro

---
+++ server/registro/src/registro.exemplo.js
@@ -0,0 +1,30 @@
+humano = {
+  local: 'Vitória da Conquista',
+  latitude: -14.869949421292642,
+  longitude: -40.826193143519184,
+  altitude: 923,
+  dataHora: '2021-07-03 00:45:00',
+  observador: 'João',
+  email: 'joao@email.com',
+  tipo: 'Em vegetação',
+  observacoes: 'Queimando lixo em lote abandonado',
+  categoria: 1,
+  tempo: 'Nublado',
+};
+
+satelite = {
+  datahora: {
+    $date: '2021-06-02T11:05:00Z',
+  },
+  satelite: 'AQUA_M-T',
+  pais: 'Brasil',
+  estado: 'ACRE',
+  municipio: 'FEIJO',
+  bioma: 'Amazonia',
+  diasemchuva: 3,
+  precipitacao: 0,
+  riscofogo: 0.3,
+  latitude: -8.344,
+  longitude: -70.109,
+  frp: 6.5,
+};
