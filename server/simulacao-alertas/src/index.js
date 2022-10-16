e413b1c97c391ab910aa2f638036e56cfa6e0dd2
João Pedro Luz <joao.pedro.luz@hotmail.com>
2022-10-16 20:10:00 -0300

    Adicionada função para contar o tempo de execução.

--- server/simulacao-alertas/src/index.js
+++ server/simulacao-alertas/src/index.js
@@ -1,80 +1,16 @@
-import dotenv from 'dotenv/config.js';
 import { faker } from '@faker-js/faker/locale/pt_BR';
 import fetch from 'node-fetch';
 import alertas from './satelite2021.json' assert { type: 'json' };
+import dotenv from 'dotenv/config';
 
-// const listaSatelites = ['Aqua Tarde', 'Terra Manhã', 'GOES-16'];
-// const listaCategoria = [
-//   'Lento ou em chamas',
-//   'Rasteiro',
-//   'Ativo',
-//   'Tocha',
-//   'Copa',
-//   'Irregular e extremo',
-// ];
-// const listaCaracteristicas = ['Em fogueira', 'Em vegetação', 'Em linha'];
-// const listaTempo = [
-//   'Céu claro',
-//   'Névoa ou fumaça',
-//   'Névoa úmida',
-//   'Nevoeiro',
-//   'Chuvisco',
-//   'Chuva',
-//   'Chuva trovoada',
-// ];
-
-// let alerta = {
-//   local: '',
-//   latitude: '',
-//   longitude: '',
-//   altitude: '',
-//   dataHora: '',
-//   categoria: '',
-//   caracteristicas: '',
-//   tempo: '',
-//   satelite: '',
-// };
-
-// const gerarAlerta = (satelite) => {
-//   alerta.local = faker.address.cityName();
-//   alerta.latitude = faker.address.latitude();
-//   alerta.longitude = faker.address.longitude();
-//   alerta.altitude = faker.datatype.number({ min: 0, max: 1600 });
-//   alerta.dataHora = faker.date.recent();
-//   alerta.categoria =
-//     listaCategoria[
-//       faker.datatype.number({ min: 0, max: listaCategoria.length - 1 })
-//     ];
-//   alerta.caracteristicas =
-//     listaCaracteristicas[
-//       faker.datatype.number({ min: 0, max: listaCaracteristicas.length - 1 })
-//     ];
-//   alerta.tempo =
-//     listaTempo[faker.datatype.number({ min: 0, max: listaTempo.length - 1 })];
-
-//   if (satelite) {
-//     alerta.satelite =
-//       listaSatelites[
-//         faker.datatype.number({ min: 0, max: listaSatelites.length - 1 })
-//       ];
-//     return emitirAlerta(alerta, true);
-//   }
-
-//   return emitirAlerta(alerta, false);
-// };
-
-// gerarAlerta(false);
-
+// Método que simula a conexão com um servidor externo.
 async function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-async function emitirAlerta(alerta, satelite) {
-  const url = satelite
-    ? `${process.env.REGISTRO_API}/alertas/satelite`
-    : `${process.env.REGISTRO_API}/alertas/humano`;
-
-    console.log(url)
+// Método que emite o alerta para o back-end.
+async function emitirAlerta(alerta) {
+  const url = `${process.env.REGISTRO_API}/alertas/satelite`;
 
   await fetch(url, {
     method: 'POST',
@@ -86,17 +22,33 @@ async function emitirAlerta(alerta, satelite) {
   });
 }
 
+// Método que seleciona um alerta aleatoriamente na lista de alertas.
 async function simularAlerta(quantidade) {
   for (let i = 0; i < quantidade; i++) {
     const alertaSorteado = faker.datatype.number({ min: 0, max: 184081 });
 
+    // Remove o identificador do alertas para que o banco possa criar um novo.
     const alerta = alertas[alertaSorteado];
     delete alerta['_id'];
 
-    await sleep(3000);
+    // await sleep(3000);
 
-    await emitirAlerta(alerta, true);
+    await emitirAlerta(alerta);
   }
 }
 
-simularAlerta(5);
+// Função que conta o tempo que levou para executar o número de alertas recebidos no parâmetro.
+async function tempoDeExecução(quantidadeDeAlertas) {
+  const start = new Date().getTime();
+  await simularAlerta(quantidadeDeAlertas);
+  const end = new Date().getTime();
+
+  let tempoTotal = end - start;
+  tempoTotal = Math.round(tempoTotal / 1000);
+  console.log(
+    `Tempo total para simular ${quantidadeDeAlertas} alertas: ${tempoTotal} segundos.`
+  );
+}
+
+// Ao chamar a função tempo de execução é necessário comentar a chamada do sleep na linha 33.
+tempoDeExecução(1000);
